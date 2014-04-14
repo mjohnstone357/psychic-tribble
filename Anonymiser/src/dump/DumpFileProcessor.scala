7ef880c876452b9fe894ef3f93344cfc64b6f5eb
mjohnstone357 <mjohnstone357@gmail.com>
2014-04-14 20:18:55 +0100

    Updated main method to read table data.

--- Anonymiser/src/dump/DumpFileProcessor.scala
+++ Anonymiser/src/dump/DumpFileProcessor.scala
@@ -9,18 +9,34 @@ object DumpFileProcessor {
 
   def main(args: Array[String]) = {
 
-    val tables = new mutable.MutableList[DatabaseTable]()
-
     val file: BufferedSource = Source.fromFile("/home/matt/firstTwo.sql")
 
-    var linesIterator: Iterator[String] = file.getLines()
+    val linesIterator: Iterator[String] = file.getLines()
 
-    while (linesIterator.hasNext) {
+    val tables: Set[DatabaseTable] = readTableData(linesIterator)
+
+    file.close()
+
+    println("Got " + tables.size + " table(s):")
+
+    println("Next line starts with 'INSERT INTO'? " + (if (linesIterator.next().startsWith("INSERT INTO")) "yes" else "no"))
+
+  }
+
+  def readTableData(iterator: Iterator[String]): Set[DatabaseTable] = {
+
+    val tables = new mutable.MutableList[DatabaseTable]()
+
+    var linesIterator = iterator
+    
+    var reachedInsertStatements = false
+
+    while (linesIterator.hasNext && !reachedInsertStatements) {
       var parserMatched = false
 
       var i = 0
 
-      while (i < Parsers.AllParsers.length && !parserMatched) {
+      while (i < Parsers.AllParsers.length && !parserMatched && !reachedInsertStatements) {
         val result: ParseStatusResult = Parsers.AllParsers(i).tryParse(linesIterator)
         result match {
           case NoParse(linesToReturn) => linesIterator = new ReclaimingIterator(linesToReturn, linesIterator)
@@ -32,16 +48,18 @@ object DumpFileProcessor {
             }
         }
         i += 1
+
+        val candidateInsertLine: String = linesIterator.next()
+        if (candidateInsertLine.startsWith("INSERT INTO")) {
+          println("Found INSERT INTO statement. Bailing out.")
+          reachedInsertStatements = true
+        } else {
+          linesIterator = new ReclaimingIterator(List(candidateInsertLine), linesIterator)
+        }
+
       }
-
-      assert(parserMatched, "No parser matched the input: " + linesIterator.next)
-
     }
-
-    file.close()
-
-    println("Got " + tables.size + " table(s):")
-
+    tables.toSet
   }
 
 }
